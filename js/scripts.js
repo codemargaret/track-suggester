2a22bcb7c730ac680f2f9509186aa4611896263a
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-08-11 10:10:58 -0700

    add variables in js to capture user answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
   event.preventDefault();
+    var design = $("select#design").val();
+    var fast = $("select#fast").val();
+    var focus = $("select#focus").val();
+    var size = $("select#size").val();
+    var bustype = $("select#bustype").val();
   });
 });
