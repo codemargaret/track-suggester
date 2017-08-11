2797092bb76e6505340cb9b23d06de1e5f0a3864
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-08-11 10:44:30 -0700

    add js for css track

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,18 @@ $(document).ready(function() {
     var focus = $("select#focus").val();
     var size = $("select#size").val();
     var bustype = $("select#bustype").val();
+
+    if (design === 'yes' && fast === 'no' || design === 'yes' && focus === 'looks' || focus === 'looks' && fast === 'no') {
+      $('#css').show();
+      $("#ruby,#csharp").hide();
+    }
+    // else if (gender === 'male' && niceness === 'mean') {
+    //   $('#dwight').show();
+    //   $("#jim, #pam, #angela").hide();
+    //     }
+    else {
+      $('#ruby').show();
+      $("#css,#csharp").hide();
+    }
   });
 });
